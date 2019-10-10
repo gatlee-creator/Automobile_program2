cf8eccf128e811200b9b2660f81c43f6aea199f2
gatlee-creator <gatlee@highpoint.edu>
2019-10-10 17:41:49 -0400

    latest version

--- testdrive.cpp
+++ testdrive.cpp
@@ -8,9 +8,10 @@ int main(){
     cout << "CAR1: creating car1 with parameterized constructor." << endl;
     automobileType car1(12000, 21, 20.5, 25);
     cout << car1.getInfoString() << endl;
-    cout << "driving 30 miles" << endl;
-    car1.drive(30);
-    cout << car1.getInfoString() << endl;
+    cout << "CAR1: calling setProperties and passing an invalid fuelInput" << endl;
+    car1.setProperties(12000, 27, 20.5); 
+
+    
 
     return 0;
 }
