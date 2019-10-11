113bf4f40b56b09c94ab6f6e061950906be992f3
gatlee-creator <gatlee@highpoint.edu>
2019-10-10 22:07:05 -0400

    function prototypes are fully documented

--- automobile.h
+++ automobile.h
@@ -22,17 +22,19 @@ class automobileType{
 
         void setProperties(float, float, float);
         /*
-            Function takes 3 parameters to set the odemeter, fuel, and effiency.
-            If any values are below 0 then they are automatically set to 0
+            Function takes 3 float parameters to set the odemeter, fuel, and effiency.
             Post Condtion:
                 odemeter = odmter
                 fuel = fuelLevel
                 efficieny = effo
+            Conditionals are in place to check if the input values are within the 
+            permitted range. If any input values are invalid then none of 
+            the values are set.
         */
         std::string getInfoString() const;
         /*
-            Function that returns a string with an update of all 3 properties.
-            private members are not mutated, but have copies that are converted
+            Function that returns a string with a current update of all 3 properties.
+            Private members are not mutated, but have copies that are converted
             to strings. 
             Post Condition:
                 "Miles = odemeter, Fuel = fuelLevel, efficieny = effo"
@@ -62,10 +64,8 @@ class automobileType{
             nor lower than the current value.
             Post Condition:
                 odemeter = odmtrInput
-            If the arguement passed in violates any of the conditions then -1 is returned
-            and a default value is set to odemeter.
+            If the arguement passed violates any of the conditions then -1 is returned.å
             Post Condition:
-                odemeter = DEFAULT_ODEMETER
                 return -1 
 
         */
@@ -76,10 +76,8 @@ class automobileType{
             the tank's max capacity and is not a negative input value. 
             Post Condition:
                 fuel = fuelInput
-            If the arguement passed in violates any of the conditions then -1 is returned
-            and a default value is set to fuel.
-            Post Condition:
-                fuel = DEFAULT_FUEL_LEVEL
+            If the arguement passed in violates any of the conditions then -1 is returned.
+            Post Condition:å
                 return -1 
         
         */
@@ -90,35 +88,53 @@ class automobileType{
             below 1.
             Post Condition:
                 efficiency = effoInput
-            If the arguement passed in violates any of the conditions then -1 is returned
-            and a default value is set to efficiency. 
-            Post Condition:
-                efficiency = DEFAULT_EFFICIENY
+            If the arguement passed in violates any of the conditions then -1 is returned.
+            Post Condition:å
                 return -1 
         */
         void refillTank(float);
-        
+        /*
+            This function takes a float parameter and adds to the current fuel.
+            In turn this refills the fuel tank. 
+            Post Condition:
+                fuel += fuelInput
+            Conditionals check if the input value exceeds the tank capacity or
+            if it is below 1. 
+        */
         void drive(float);
         /*
-            Function takes a parameter for amount of miles the car has driven.
+            Function takes a parameter for amount of miles the car is to drive.
             The function then calculates the new fuel level and updates the
             odemeter. 
             Post Condition:
                 fuel -= gallonsUsed
                 odemeter += milesTrav
+            Function also calculates the possible driving range of the car. If
+            the input value exceeds this then the car will run out of fuel. In 
+            like manner,the odemeter only increases by the actual number 
+            of miles driven.   
         */
         automobileType();
         /*
             Default constructor for automobileType. 
-            Calls the setProperties function to set all to 0
+            DEFAULTS_VALUES are assigned to each of the private variables.
             Post Condition:
-                setProperties(0,0,0)
+                  odemeter = DEFAULT_ODEMETER
+                  fuel = DEFAULT_FUEL_LEVEL
+                  efficiency = DEFAULT_EFFICIENY
+                  maxFuel = DEFAULT_MAX_FUEL
         */
         automobileType(float, float, float, float);
         /*
-            Constructor that takes parameters to initialize automobileType
-            Post Concdtion:
-                setProperties(float, float , float)
+            Constructor that takes 3 parameters to initialize automobileType. 
+            The setFunctions are called to set the properties odemeter, fuel, efficiency.
+            Post Condtion:
+                if(setFuelLevel(fuelInput) == INVALID)
+                if(setOdemeter(odmtrInput) == INVALID)
+                if(setEfficiency(effoInput) == INVALID)
+            The conditional wrapped around the setFunctions are used so that any/all 
+            invalid parameters are set to DEFAULT_VALUES to ensure the constructed 
+            object is immediately usable.
         */
 
     private:
